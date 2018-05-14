581c18575beccf96eb79bfc4cb9d90eda2e48373
nadia <34341952+nkarimi00@users.noreply.github.com>
2018-05-14 16:26:43 +0200

    Added feature get studentenhuis and test

--- routes/routes_api.js
+++ routes/routes_api.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const auth =  require('../auth/authentication');
+const auth = require('../auth/authentication');
 const db = require("../datasource/mysql-connector");
 
 //
 // Catch all except login
 //
-router.all( new RegExp("[^(\/register)]"), function (req, res, next) {
+// router.all( new RegExp("[^(\/register)]"), function (req, res, next) {
 
-    //TODO register using email, first/last name and password
-});
-router.all( new RegExp("[^(\/login)]"), function (req, res, next) {
+//     //TODO register using email, first/last name and password
+// });
+router.all(new RegExp("[^(\/login)]"), function (req, res, next) {
     //
     console.log("VALIDATE TOKEN");
     var token = (req.header('X-Access-Token')) || '';
     auth.decodeToken(token, (err, payload) => {
         if (err) {
             console.log('Error handler: ' + err.message);
-            res.status((err.status || 401 )).json({error: new Error("Not authorised").message});
+            res.status(401).json({ error: new Error("Not authorised").message });
         } else {
             next();
         }
@@ -30,7 +30,7 @@ router.all( new RegExp("[^(\/login)]"), function (req, res, next) {
 //
 router.route('/login')
 
-    .post( function(req, res) {
+    .post(function (req, res) {
 
         //
         // Get body params or ''
@@ -47,21 +47,54 @@ router.route('/login')
         //     timeout: 40000, 
         //     values: [email]});
         result = users.filter(function (user) {
-            if( user.email === email && user.password === password) {
-                return ( user );
+            if (user.email === email && user.password === password) {
+                return (user);
             }
         });
 
         // Debug
-        console.log("result: " +  JSON.stringify(result[0]));
+        console.log("result: " + JSON.stringify(result[0]));
 
         // Generate JWT
-        if( result[0] ) {
-            res.status(200).json({"token" : auth.encodeToken(username), "username" : username});
+        if (result[0]) {
+            res.status(200).json({ "token": auth.encodeToken(username), "username": username });
         } else {
-            res.status(401).json({"error":"Invalid credentials, bye"})
+            res.status(401).json({ "error": "Invalid credentials, bye" })
         }
 
+    });
+// Get all studentenhuizen
+router.get('/studentenhuis', (req, res, next) => {
+
+    pool.getConnection(function (err, connection) {
+        connection.query('SELECT * FROM studentenhuis',
+            (error, rows) => {
+                if (error) {
+                    res.status(500).json(error.toString())
+                    connection.release();
+                } else {
+                    res.status(200).json(rows)
+                    connection.release();
+                }
+            });
+    });
+});
+
+router.get('/studentenhuis/:id', (req, res, next) => {
+
+    const huisId = req.params.id;
+
+    pool.getConnection(function (err, connection) {
+        connection.query('SELECT * FROM studentenhuis WHERE ID = ?',
+            [huisId],
+            (error, rows, fields) => {
+                if (error) {
+                    res.status(500).json(error.toString())
+                } else {
+                    res.status(200).json(rows)
+                }
+            })
+    });
 });
 
 module.exports = router;
