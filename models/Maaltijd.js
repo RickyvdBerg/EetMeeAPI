e5d3d1a61fd0eb24239ba25f4dd46f92329d6f00
Ricky van den berg <ricky.vandenberg@hotmail.nl>
2018-05-14 14:03:53 +0200

    updated models

---
+++ models/Maaltijd.js
@@ -0,0 +1,12 @@
+class Maaltijd {
+
+    constructor(name, description, ingredients, allergy, price)
+    {
+        this.name = name;
+        this.adres = adres;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.allergy = allergy;
+        this.price = price;
+    }
+}
