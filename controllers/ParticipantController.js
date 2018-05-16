abb35b8abc232f35abe24171307954e88de4c1fe
Ricky van den berg <ricky.vandenberg@hotmail.nl>
2018-05-16 18:30:43 +0200

    user can now only participate in a meal if he did not register already

--- controllers/ParticipantController.js
+++ controllers/ParticipantController.js
@@ -10,23 +10,32 @@ module.exports = {
 
         const houseId = req.params.id;
         const mealId = req.params.mid;
-        
 
-        db.query('INSERT INTO deelnemers (UserID, StudentenhuisID, MaaltijdID) VALUES ((SELECT ID from user where Email = ?),?,?)',
+        db.query('SELECT * FROM deelnemers WHERE UserID = (SELECT ID from user where Email = ?) AND StudentenhuisID = ? AND MaaltijdID = ?',
             [email, houseId, mealId],
-            (error, result, fields) => {
-                console.log(error);
-                if(!result){
-                    res.status(404).json({ "error": "one or more params incorrect" })
-                }
-                if (result.affectedRows == 0) {
-                    res.status(412).json({ "error": "No changes were made an error occured" })
-                }
-                else {
-                    res.status(200).json({ "succes": "You registered for this meal" })
-                }
-            })
-            //TODO check if already subscribed to meal
+            (error, result) => {
+                if (!result[0]) {
+                    db.query('INSERT INTO deelnemers (UserID, StudentenhuisID, MaaltijdID) VALUES ((SELECT ID from user where Email = ?),?,?)',
+                        [email, houseId, mealId],
+                        (errorInner, resultInner) => {
+                            if (!resultInner) {
+                                res.status(404).json({ "error": "one or more params incorrect " + result })
+                                return
+                            }
+                            if (resultInner.affectedRows == 0) {
+                                res.status(412).json({ "error": "No changes were made an error occured" })
+                            }
+                            else {
+                                res.status(200).json({ "succes": "You registered for this meal" })
+                            }
+                    
+                        })
+                    }
+                    else{
+                        res.status(409).json({ "error": "already registered for this meal" })
+                    }
+                })
+        //TODO check if already subscribed to meal
     },
     getParticipantsForMeal(req, res, next) {
         const houseId = req.params.id;
@@ -53,21 +62,20 @@ module.exports = {
         const mealId = req.params.mid;
 
         db.query('DELETE deelnemers FROM deelnemers INNER JOIN user ON deelnemers.UserID = user.ID WHERE user.Email = ? AND deelnemers.MaaltijdID = ? AND deelnemers.StudentenhuisID = ?',
-        [email, mealId,houseId],
-        (errorInner, resultInner, fieldsInner) => {
-            if(errorInner)
-            {
-                res.status(500).json({"error" : "Something went wrong while trying to delete entry from meals"})
-            }
-            if(!resultInner){
-                res.status(404).json({ "error": "one or more params incorrect" })
-            }
-            if (resultInner.affectedRows > 0) {
-                res.status(200).json({ "succes" : "Changes were made"});
-            }
-            else{
-                res.status(409).json({ "error": "Could not delete participant, are you the owner of this record?" })
-            }
-        })
+            [email, mealId, houseId],
+            (errorInner, resultInner, fieldsInner) => {
+                if (errorInner) {
+                    res.status(500).json({ "error": "Something went wrong while trying to delete entry from meals" })
+                }
+                if (!resultInner) {
+                    res.status(404).json({ "error": "one or more params incorrect" })
+                }
+                if (resultInner.affectedRows > 0) {
+                    res.status(200).json({ "succes": "Changes were made" });
+                }
+                else {
+                    res.status(409).json({ "error": "Could not delete participant, are you the owner of this record?" })
+                }
+            })
     },
 }
